0752590cbc78c75088007eb8b4ba276b0ff7123a
bluesanta <bluesanta@DESKTOP-CA82AM5>
2018-11-08 23:39:43 +0900

    최초등록

---
+++ math.cpp
@@ -0,0 +1,15 @@
+#include "pxt.h"
+
+//% color=50 weight=80
+//% icon="\uf1eb"
+namespace MATH { 
+
+  /**
+  * button pushed.
+  */
+  //% blockId=math_sum
+  //% block="sum = %src1 + %src2"
+  int sum(int src1, int src2) {
+      return src1 + src2;
+  }
+}
---
+++ shims.d.ts
@@ -0,0 +1,17 @@
+// Auto-generated. Do not edit.
+
+
+
+    //% color=50 weight=80
+    //% icon="\uf1eb"
+declare namespace MATH {
+
+    /**
+     * button pushed.
+     */
+    //% blockId=math_sum
+    //% block="sum = %src1 + %src2" shim=MATH::sum
+    function sum(src1: int32, src2: int32): int32;
+}
+
+// Auto-generated. Do not edit. Really.
